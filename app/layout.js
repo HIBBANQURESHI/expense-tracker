01b1458e1589049500c648e8d0b105b0bb015e77
HIBBANQURESHI <qureshihibban@gmail.com>
2025-03-04 02:47:52 +0500

    added

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import Sidebar from "../components/Sidebar/index.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "./globals.css";
+import "./globals.css"; // ✅ This is correct!
 import { Poppins } from 'next/font/google';
 
 const poppins = Poppins({
@@ -13,9 +13,6 @@ const poppins = Poppins({
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <head>
-                <link rel="preload" href="../styles/globals.css" as="style" />
-            </head>
             <body className={`flex min-h-screen bg-white text-black ${poppins.className}`}>
                 <Sidebar />
                 <main className="flex-1 p-6 overflow-auto">
