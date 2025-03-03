6d6983aa702ff6dcce57e1c333e6cc8a8f14b847
HIBBANQURESHI <qureshihibban@gmail.com>
2025-03-04 02:32:07 +0500

    added

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,21 @@
 import Sidebar from "../components/Sidebar/index.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "./globals.css";
+import "../styles/globals.css";
 import { Poppins } from 'next/font/google';
 
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['100', '200', '300', '400', '500', '600', '700'],
+  display: 'swap', // Helps with FOUT issues
 });
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <head>
+                <link rel="preload" href="../styles/globals.css" as="style" />
+            </head>
             <body className={`flex min-h-screen bg-white text-black ${poppins.className}`}>
                 <Sidebar />
                 <main className="flex-1 p-6 overflow-auto">
