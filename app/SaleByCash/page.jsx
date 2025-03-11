da8b365531e96a9a170f699f109f652862f99c31
HIBBANQURESHI <qureshihibban@gmail.com>
2025-03-12 01:34:43 +0500

    added

--- app/SaleByCash/page.jsx
+++ app/SaleByCash/page.jsx
@@ -291,7 +291,10 @@ const SaleByCash = () => {
                       <td className="px-4 py-3 text-sm space-x-2">
                       <button
                         className="text-blue-600 hover:text-blue-800"
-                         onClick={() => router.push(`/SaleByCash/${sale._id}`)}
+                        onClick={() => {
+                          console.log('Navigating to ID:', sale._id);
+                          router.push(`/SaleByCash/${sale._id}`);
+                        }}
                       >
                          Edit
                       </button>
